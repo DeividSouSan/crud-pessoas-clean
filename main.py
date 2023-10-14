6806fccfd4e2de0013e6a27a3a75a18664e38632
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-10-14 10:48:45 -0300

    chore: example code

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 from models.database import Database
 from factories.media_factory import MediaFactory
-from models.interface import Interface
+from controller.interface import Interface
+import os
+
+"""
+You can implement any commands you want in the interface, those below are just some examples.
+"""
+
+
+def addBookToDatabase():
+    media = Media.createMedia(
+        input('Tipo: '), input('Titulo: '), input('Autor: '))
+    db.create(2, media.toDict())
+
+
+def readIndexFromDatabase():
+    indexList = db.read('keys')
+    indexList = sorted(list(map(int, indexList)))
+    return indexList
+
+
+def sumAllIndex():
+    indexList = readIndexFromDatabase()
+    return print(sum(indexList))
+
 
 db = Database()
-db.set('peopleList')
+db.set(os.getenv('DATABASE_FILE'))
 
 Media = MediaFactory()
-media1 = Media.createMedia('book', 'Tartarugas Até Lá em Baixo', 'John Green')
 
-db.create(2, media1.toDict())
+i = Interface()
+i.commands = {
+    "create": {"func": addBookToDatabase, "parameters": 0},
+    "read index": {"func": readIndexFromDatabase, "parameters": 0},
+    "sum indexes": {"func": sumAllIndex, "parameters": 0}
+}
 
-
-'Interface -- (dados) -> MediaFactory -- (dict) --> Database'
+r = i.listen()
+i.execute(r)
